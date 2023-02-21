d981ad5059dc75b1826368e7d4d8721d84286c5b
Dhruv Bhola <dharuvbhola@gmail.com>
2023-02-21 10:06:24 +0530

    test added

---
+++ test.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class test { 
+    public static void main(String[] args) {
+
+        char i = 'c';
+        int y = 7;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println(++y*8);
+    }
+}
+
+
